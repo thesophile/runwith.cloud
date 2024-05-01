9b136f9f9c962b259359ded11409efe1a484ed6d
thesophile <abhinavpb249@gmail.com>
2024-05-01 14:57:21 +0530

    edit: allowed hosts

--- cloudpy/settings.py
+++ cloudpy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-bm5v%1eo(9ctdvd5!=anucw^8dn!^jd%zye%y_j%f@lz$b^_*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
