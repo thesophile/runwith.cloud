172585b772a495d406b553bcc01251363726e604
thesophile <abhinavpb249@gmail.com>
2024-06-22 12:18:04 +0530

    Textarea nowrap

--- cloudpy/settings.py
+++ cloudpy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-bm5v%1eo(9ctdvd5!=anucw^8dn!^jd%zye%y_j%f@lz$b^_*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cloudpy.online','www.cloudpy.online']
+ALLOWED_HOSTS = ['cloudpy.online','www.cloudpy.online','127.0.0.1']
 
 
 # Application definition
