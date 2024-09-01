bcbe6d8c17f8c59babe444f66ddbefddba85614f
thesophile <abhinavpb249@gmail.com>
2024-09-01 14:28:52 +0530

    placeholder fix

--- manim/cache_utils.py
+++ manim/cache_utils.py
@@ -7,7 +7,12 @@ def save_to_cache(code):
 
 def get_previous_code():
     code = cache.get('previous_code')
-    return code if code is not None else ''
+    placeholder_code = '''from manim import *
+
+# enter your manim code here '''
+ 
+    
+    return code if code is not None else placeholder_code
 
 #Current Code (A variable to keep track which code is crrently being edited)
      
