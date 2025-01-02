9a3f073617f4860cfc32dbdacf4775ebd58ea816
abhinavpbabu <abhinavbabu.contact@gmail.com>
2025-01-02 13:55:16 +0530

    add asterick to form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,35 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe  # Required for rendering HTML in labels
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label=mark_safe("Username <span style='color: red;'>*</span>")  # Red asterisk
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label=mark_safe("Password <span style='color: red;'>*</span>")  # Red asterisk
+    )
 
 class SignupForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label=mark_safe("Password <span style='color: red;'>*</span>")  # Red asterisk
+    )
+    password_confirm = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label=mark_safe("Confirm Password <span style='color: red;'>*</span>")  # Red asterisk
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password_confirm']
+        labels = {
+            'username': mark_safe("Username <span style='color: red;'>*</span>"),
+            'email': mark_safe("Email <span style='color: gray;'>(optional)</span>"),  # Optional styling
+        }
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
