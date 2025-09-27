8bb2f07b37bf20d7caad6693dffda9e85049a7df
Abhinav Babu <abhinavbabu.contact@gmail.com>
2025-09-27 17:56:14 +0530

    host access edit
    change this after testing

--- cloudpy/settings.py
+++ cloudpy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-bm5v%1eo(9ctdvd5!=anucw^8dn!^jd%zye%y_j%f@lz$b^_*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cloudpy.online','www.cloudpy.online','127.0.0.1','13.61.19.243']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -145,3 +145,4 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
